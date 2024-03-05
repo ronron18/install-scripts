df3c07e7fda316656acaf0e99fec4f66d8b5bcfd
ronron18 <ronankonstantin@gmail.com>
2024-03-05 13:52:35 +0800

    Fixed graylog installation script

--- graylog/install_graylog_5.2_part1.sh
+++ graylog/install_graylog_5.2_part1.sh
@@ -8,11 +8,15 @@
 # opensearch hates easy default passwords, so this is the initial admin password. You should change it later
 # for security reasons
 OPENSEARCH_PASSWORD="V3rY5tr0nGP@ssW0rD!"   
+echo "Enter the non-root user: "
+read default_user
 
 # Make sure that proxmox cpu type is set to host
 
 # Add sbin paths
+chmod 666 /root/.bashrc
 echo "export PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin" >> /root/.bashrc
+chmod 644 /root/.bashrc
 
 # Install sudo and add specified user to group.
 apt-get install sudo
@@ -37,8 +41,11 @@ systemctl restart mongod.service
 systemctl status mongod.service
 
 # Here we use opensearch instead of elasticsearch as recommended by graylog
-wget https://artifacts.opensearch.org/releases/bundle/opensearch/2.12.0/opensearch-2.12.0-linux-x64.deb
+wget https://artifacts.opensearch.org/releases/bundle/opensearch/2.9.0/opensearch-2.9.0-linux-x64.deb
 
-echo -e "OPENSEARCH_INITIAL_ADMIN_PASSWORD=$OPENSEARCH_PASSWORD\nOPENSEARCH_JAVA_HOME=/usr/share/opensearch/jdk"
+chmod 666 /etc/environment
+echo -e "OPENSEARCH_INITIAL_ADMIN_PASSWORD=$OPENSEARCH_PASSWORD\nOPENSEARCH_JAVA_HOME=/usr/share/opensearch/jdk" >> /etc/environment
+chmod 644 /etc/environment
+echo "Please reboot your system"
 
 # reboot then run part 2
--- graylog/install_graylog_5.2_part2.sh
+++ graylog/install_graylog_5.2_part2.sh
@@ -9,20 +9,27 @@
 # This is part 2 of the graylog installation script. this part will install opensearch and graylog server into the system.
 # Please make sure part 1 is done already.
 
-JVM_RAM=4
-ip_address=""   # Make this an input
+echo "Input the amount of RAM you want to allocate for opensearch JVM (Typically half of your system memory): "
+read JVM_RAM
+echo -e "\nInput the IP address of the server"
+read ip_address
 
-dpkg -i opensearch-2.12.0-linux-x64.deb
+dpkg -i opensearch-2.9.0-linux-x64.deb
 
 systemctl enable opensearch
 systemctl start opensearch
 systemctl status opensearch
 
-echo -e "\ncluster.name: graylog\nnode.name: ${HOSTNAME}\ndiscovery.type: single-node\nnetwork.host: 0.0.0.0\naction.auto_create_index: false\nplugins.security.disabled: true\nindices.query.bool.max_clause_count: 32768"
+
+chmod 666 /etc/opensearch/opensearch.yml
+echo -e "\ncluster.name: graylog\nnode.name: \${HOSTNAME}\ndiscovery.type: single-node\nnetwork.host: 0.0.0.0\naction.auto_create_index: false\nplugins.security.disabled: true\nindices.query.bool.max_clause_count: 32768" >> /etc/opensearch/opensearch.yml
+chmod 644 /etc/opensearch/opensearch.yml
 
 # Set jvm RAM
-sed -i "s/-Xms1g/-Xms${JVM_RAM}g" /etc/opensearch/jvm.options
-sed -i "s/-Xmx1g/-Xmx${JVM_RAM}g" /etc/opensearch/jvm.options
+chmod 666 /etc/opensearch/jvm.options
+sed -i "s/-Xms1g/-Xms${JVM_RAM}g/" /etc/opensearch/jvm.options
+sed -i "s/-Xmx1g/-Xmx${JVM_RAM}g/" /etc/opensearch/jvm.options
+chmod 644 /etc/opensearch/jvm.options
 
 sysctl -w vm.max_map_count=262144
 echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
@@ -34,16 +41,21 @@ systemctl start opensearch
 # Install graylog
 wget https://packages.graylog2.org/repo/packages/graylog-5.2-repository_latest.deb
 dpkg -i graylog-5.2-repository_latest.deb
-apt-get update && apt-get install graylog-server
+apt-get update
+apt-get install graylog-server
 
 
 # Configure graylog server configuration file
+chmod 666 /etc/graylog/server/server.conf
 # Set password secret
-sed -i "s/password_secret = /password_secret = $(< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c${1:-96};echo;)" /etc/graylog/server/server.conf
+sed -i "s/password_secret =/password_secret = $(< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c${1:-96};echo;)/" /etc/graylog/server/server.conf
 # root password sha2
 echo "Please enter the Graylog server root password: "
-sed -i "s/root_password_sha2 = /root_password_sha2 = $(echo -n "" && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1)" /etc/graylog/server/server.conf
+sed -i "s/root_password_sha2 =/root_password_sha2 = $(echo -n "" && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1)/" /etc/graylog/server/server.conf
+# change to sed
 echo "http_bind_address = ${ip_address}:9000" >> /etc/graylog/server/server.conf
+echo "elasticsearch_hosts = http://${ip_address}:9200" >> /etc/graylog/server/server.conf
+chmod 644 /etc/graylog/server/server.conf
 
 # Start graylog server
 systemctl daemon-reload
