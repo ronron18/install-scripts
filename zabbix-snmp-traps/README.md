eaaf1df0f3fbed2e07eac249579dec7aedb80bed
ronron18 <ronankonstantin@gmail.com>
2024-03-14 13:36:38 +0800

    Added zabbix snmp trap installer (untested) and a readme for that.

---
+++ zabbix-snmp-traps/README.md
@@ -0,0 +1,10 @@
+# Zabbix Trap Installation
+## Instructions
+1. Make sure Zabbix is installed
+2. Get the files on the target system.
+    - This can be done via various methods such as the `scp` command, via an external storage device, etc.
+3. Get the perl script here ``https://git.zabbix.com/projects/ZBX/repos/zabbix/raw/misc/snmptrap/zabbix_trap_receiver.pl`` and put it in the same directory as the script.
+    - Use `scp`
+4. Give execute permissions to the files with `chmod u+x [file_name]` .
+5. Execute installation script and follow the instructions.
+8. You're done, what is left is manual configuration to your likings.
--- zabbix-snmp-traps/zabbix_snmp_trap_setup.sh
+++ zabbix-snmp-traps/zabbix_snmp_trap_setup.sh
@@ -7,39 +7,50 @@
 #  StartSNMPTrapper=1
 #  SNMPTrapperFile=[TRAP_FILE (/var/log/snmptrap/snmptrap.log)]
 #  Give SNMPTrapperFile write permissions
+echo "---===---   Configuring Zabbix to receive SNMP traps   ---===---"
 chmod 666 /etc/zabbix/zabbix_server.conf
 echo "StartSNMPTrapper=1" >> /etc/zabbix/zabbix_server.conf
 chmod 644 /etc/zabbix/zabbix_server.conf
 mkdir /var/log/snmptrap/
+echo "" > /var/log/snmptrap/snmptrap.log
 chmod 666 -R /var/log/snmptrap/
 
-## Configure SNMPTT
-apt-get install snmptt
-chmod 666 /etc/snmp/snmptt.ini
-sed -i "s/net_snmp_perl_enable = 0/net_snmp_perl_enable = 1/" /etc/snmp/snmptt.ini
-sed -i "s/log_file = \\/var\\/log\\/snmptt\\/snmptt.log/log_file = \\/var\\/log\\/snmptrap\\/snmptrap.log/" /etc/snmp/snmptt.ini
-sed -i "s/#date_time_format =/date_time_format = %H:%M:%S %Y\\/%m\\/%d/" /etc/snmp/snmptt.ini
-#echo "date_time_format = %H:%M:%S %Y/%m/%d" >> /etc/snmp/snmptt.ini
-chmod 644 /etc/snmp/snmptt.ini
+## Open port 162
 
-chmod 666 /etc/snmp/snmptt.conf
-sed -i "s/FORMAT/FORMAT ZBXTRAP \$aA/" /etc/snmp/snmptt.conf
-chmod 644 /etc/snmp/snmptt.conf
+## Install zabbix trap receiver perl script
+# wget https://git.zabbix.com/projects/ZBX/repos/zabbix/raw/misc/snmptrap/zabbix_trap_receiver.pl -O /usr/bin/zabbix_trap_receiver.pl
+echo "---===---   Installing perl trap receiver   ---===---"
+cp zabbix_trap_receiver.pl /usr/bin/zabbix_trap_receiver.pl
+chmod a+wx /usr/bin/zabbix_trap_receiver.pl
+sed "a/\\$SNMPTrapperFile =/\\$SNMPTrapperFile = '\\/var\\/log\\/snmptrap\\/snmptrap.log' #/" /usr/bin/zabbix_trap_receiver.pl
+apt install snmp snmptrapd
 
 ## Enable receiving SNMPv3 Traps
+echo "---===---   Configurations   ---===---"
+echo "Please enter Engine ID (Leave blank if unspecified): "
 read engine_id
+echo -e "\nPlease enter SNMPv3 Name: "
 read name
+echo -e "\nPlease enter password hash type: "
 read type
-read password
+echo -e "\nPlease enter password: "
+read -rs password
 chmod 644 /etc/snmp/snmptrapd.conf
-echo -e "createUser -e $engine_id $name $type $password AES\nauthuser log, execute $name" >> /etc/snmp/snmptrapd.conf
+
+if [ "$engine_id" != "" ]; then
+    engine_id=" -e ${engine_id}"
+fi
+echo -e "createUser$engine_id $name $type $password AES\nauthuser log, execute $name" >> /etc/snmp/snmptrapd.conf
+
+echo "perl do \"/usr/bin/zabbix_trap_receiver.pl\";" >> /etc/snmp/snmptrapd.conf
 chmod 666 /etc/snmp/snmptrapd.conf
 
-## Install MIBs
-apt install snmp-mibs-downloader
-download-mibs
-chmod 666 /etc/snmp/snmp.conf
-sed -i "s/mibs :/#mibs :/" /etc/snmp/snmp.conf
-chmod 644 /etc/snmp/snmp.conf
+apt-get install libsnmp-perl
 
+echo "---===---   Restarting services   ---===---"
 service zabbix-server restart
+service snmptrapd restart
+zabbix_server -R config_cache_reload
+
+clear
+echo "---===---   Installation Completed   ---===---"
